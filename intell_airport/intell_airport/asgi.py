12db38e816abf3ff3df686d2e50ba4d70532eec2
valenzzr <949011578@qq.com>
2023-05-20 17:04:44 +0800

    修正了settings的问题

---
+++ intell_airport/intell_airport/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for intell_airport project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "intell_airport.settings")
+
+application = get_asgi_application()
